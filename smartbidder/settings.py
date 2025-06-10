ab629d297a98c4181a67e4741bee27a7c3fba63d
ahmedQarawani3 <akarwani452@gmail.com>
2025-06-10 03:16:47 +0300

    edit22547

--- smartbidder/settings.py
+++ smartbidder/settings.py
@@ -147,7 +147,7 @@ REST_FRAMEWORK = {
         'rest_framework.filters.OrderingFilter',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20,  # عدد النتائج لكل صفحة
+    'PAGE_SIZE': 80,  # عدد النتائج لكل صفحة
 }
 
 
