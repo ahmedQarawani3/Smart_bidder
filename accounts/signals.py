af9bf7f2bedf6028d3f4a692bf82fbad4c131f95
ahmedQarawani3 <akarwani452@gmail.com>
2025-07-22 20:19:25 +0300

    agttطك

--- accounts/signals.py
+++ accounts/signals.py
@@ -99,3 +99,26 @@ def update_investor_rating_on_offer_accept(sender, instance, **kwargs):
 def update_rating_on_review(sender, instance, created, **kwargs):
     if created:
         update_user_rating(instance.reviewed)
+
+# signals.py
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth import get_user_model
+from accounts.models import Notification
+from projectOwner.models import FeasibilityStudy
+
+User = get_user_model()
+
+@receiver(post_save, sender=FeasibilityStudy)
+def notify_admin_on_ai_score(sender, instance, created, **kwargs):
+    if instance.ai_score is not None:
+        # منعت التكرار: نرسل إشعار فقط إذا السكور انحط لأول مرة أو تغير
+        if not instance._state.adding and 'ai_score' in instance.get_dirty_fields():
+            # الحصول على جميع الأدمنات
+            admins = User.objects.filter(role='admin')
+            for admin in admins:
+                Notification.objects.create(
+                    user=admin,
+                    message=f"AI score reached: {instance.ai_score} for project {instance.project.title}"
+                )
